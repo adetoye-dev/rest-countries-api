6174245309a0dd3910ca9118475f4ded2eec9cf8
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-11-02 18:48:03 +0100

    refactor api call to fetch all countries only

--- src/apis/restcountries.js
+++ src/apis/restcountries.js
@@ -1,28 +1,8 @@
 import axios from "axios";
 
-const fetchAllCountries = async () => {
+const fetchCountries = async () => {
   const response = await axios.get("https://restcountries.com/v3.1/all");
   return response.data;
 };
 
-const fetchCountry = async (countryName) => {
-  const response = await axios.get(
-    `https://restcountries.com/v3.1/name/${countryName}`
-  );
-  return response.data;
-};
-
-const fetchRegions = async (region) => {
-  const response = await axios.get(
-    `https://restcountries.com/v3.1/region/${region}`
-  );
-  return response.data;
-};
-
-const restcountries = {
-  fetchAllCountries,
-  fetchCountry,
-  fetchRegions,
-};
-
-export default restcountries;
+export default fetchCountries;
