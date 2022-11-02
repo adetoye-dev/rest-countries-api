1441e344f0e4dda730b4900eb8783688bcdae44e
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-11-02 09:09:25 +0100

    create api calls instance

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,32 @@
 import NavBar from "./NavBar";
 import "./App.css";
+import restcountries from "../apis/restcountries";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [countries, setCountries] = useState([]);
+
+  const searchCountry = async (country) => {
+    const data = await restcountries.fetchCountry(country);
+    console.log(data);
+  };
+
+  const searchRegion = async (region) => {
+    const data = await restcountries.fetchRegions(region);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    const getAllCountries = async () => {
+      const data = await restcountries.fetchAllCountries();
+      console.log(data);
+    };
+
+    // getAllCountries();
+    // searchCountry("nigeria");
+    // searchRegion("africa");
+  }, []);
+
   return (
     <>
       <NavBar />
