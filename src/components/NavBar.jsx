55cbdebc2a90f79168cf11d48d7f6d1abd7f8526
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-11-02 08:19:08 +0100

    create nav component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <div className="container nav-container">
+        <h1 className="nav-text">Where in the world?</h1>
+        <div className="mode-toggle">
+          <i class="fa-solid fa-moon"></i> Dark Mode
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
