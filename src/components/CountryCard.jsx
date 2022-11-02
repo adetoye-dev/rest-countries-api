eae2049d33794fabb3ab9015048e89c9592a0274
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-11-02 10:18:56 +0100

    create country card

---
+++ src/components/CountryCard.jsx
@@ -0,0 +1,27 @@
+import "./CountryCard.css";
+
+const CountryCard = (props) => {
+  return (
+    <div className="country-card">
+      <div className="country-card-img">
+        <img src={props.flag} alt="country flag" />
+      </div>
+      <div className="country-card-data">
+        <h2 className="country-name">{props.country}</h2>
+        <div className="country-data">
+          <div className="population">
+            <span>population:</span> {props.population}
+          </div>
+          <div className="region">
+            <span>region:</span> {props.region}
+          </div>
+          <div className="capital">
+            <span>capital:</span> {props.capital}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountryCard;
