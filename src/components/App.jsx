240e3733f275aa3ae7cf3f70c4f67e9a624bb400
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-11-03 11:06:39 +0100

    add navigation with use navigate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,31 @@
 import "./App.css";
 import Home from "./Home";
 import CountryDetail from "./CountryDetail";
-import { Routes, Route } from "react-router-dom";
+import { useNavigate, Routes, Route } from "react-router-dom";
+import restCountries from "../apis/restcountries";
+import { useEffect } from "react";
 
 const App = () => {
+  const navigate = useNavigate();
+
+  const viewCountry = async (code) => {
+    const data = await restCountries.fecthCountry(code);
+    navigate(`/countries/${data[0].name.common}`, {
+      state: { ...data[0] },
+    });
+  };
+
+  useEffect(() => {
+    viewCountry("bol");
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/countries" element={<CountryDetail />} />
+      <Route
+        path="countries/:id"
+        element={<CountryDetail viewCountry={viewCountry} />}
+      />
     </Routes>
   );
 };
