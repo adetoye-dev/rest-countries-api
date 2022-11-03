5a4ababd08e125d212839dce2ba5d5840faf353a
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-11-03 09:16:59 +0100

    move main components to <Home />

---
+++ src/components/Home.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import NavBar from "./NavBar";
+import CountryCard from "./CountryCard";
+import "./Home.css";
+import fetchCountries from "../apis/restcountries";
+import { nanoid } from "nanoid";
+import SearchBar from "./SearchBar";
+import FilterBox from "./FilterBox";
+import CountryDetail from "./CountryDetail";
+
+const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [filteredCountryList, setFilteredCountryList] = useState([]);
+  const [filter, setFilter] = useState({
+    isOn: false,
+    value: "",
+  });
+
+  useEffect(() => {
+    const getAllCountries = async () => {
+      const data = await fetchCountries();
+      console.log(data);
+      setCountries(data);
+      setFilteredCountryList(data);
+    };
+
+    getAllCountries();
+  }, []);
+
+  const findCountry = (input) => {
+    const filteredList = countries.filter((country) => {
+      return filter.isOn
+        ? (country.name.official.toLowerCase().includes(input.toLowerCase()) ||
+            country.name.common.toLowerCase().includes(input.toLowerCase())) &&
+            country.region.toLowerCase() === filter.value.toLowerCase()
+        : country.name.official.toLowerCase().includes(input.toLowerCase()) ||
+            country.name.common.toLowerCase().includes(input.toLowerCase());
+    });
+
+    setFilteredCountryList(filteredList);
+  };
+
+  const filterCountries = (region) => {
+    if (region === "") {
+      setFilter({ isOn: false, value: region });
+      setFilteredCountryList(countries);
+      return;
+    }
+    setFilter({ isOn: true, value: region });
+    const filteredList = countries.filter((country) => {
+      return country.region.toLowerCase() === region.toLowerCase();
+    });
+    setFilteredCountryList(filteredList);
+  };
+
+  const renderCountries = filteredCountryList.map((country) => {
+    return (
+      <CountryCard
+        country={country.name.official}
+        region={country.region}
+        population={country.population}
+        capital={country.capital}
+        flag={country.flags.svg}
+        key={nanoid()}
+      />
+    );
+  });
+
+  return (
+    <>
+      <NavBar />
+      <div className="search-container container">
+        <SearchBar findCountry={findCountry} />
+        <FilterBox handleChange={filterCountries} />
+      </div>
+      <main className="main container">{renderCountries}</main>
+    </>
+  );
+};
+
+export default Home;
