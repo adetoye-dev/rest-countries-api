29c2c8d6f6a591e29f098b126ce9cce2350c141a
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-11-04 18:22:23 +0100

    use strict equals

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,7 +16,7 @@ const NavBar = () => {
 
   useEffect(() => {
     localStorage.setItem("country-search-theme", theme);
-    if (theme == "dark") {
+    if (theme === "dark") {
       document.body.classList.add("dark");
     } else {
       document.body.classList.remove("dark");
@@ -28,8 +28,8 @@ const NavBar = () => {
       <div className="container nav-container">
         <h1 className="nav-text">Where in the world?</h1>
         <div className="mode-toggle" onClick={toggleTheme}>
-          <i className={theme == "light" ? "fas fa-moon" : "far fa-sun"}></i>{" "}
-          {theme == "light" ? "Dark" : "Light"} Mode
+          <i className={theme === "light" ? "fas fa-moon" : "far fa-sun"}></i>{" "}
+          {theme === "light" ? "Dark" : "Light"} Mode
         </div>
       </div>
     </nav>
